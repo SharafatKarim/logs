7c4264bf8e1077fbc6eb95f091d07853b988e94b
SharafatKarim <sharafat2004@gmail.com>
2024-05-28 09:49:52 +0600

    script a bit modified for more accurate things out there

--- posts.py
+++ posts.py
@@ -20,23 +20,27 @@ def read_first_line(file):
         return str.strip()
 
 def generate_posts():
-    for year_files in os.curdir:
-        for root, dirs, files in os.walk(year_files):
+    # for root, dirs, files in os.walk(os.curdir):
+    dirs = os.listdir(os.curdir)
+    # print(dirs)
     # for root, dirs, files in os.walk("src"):
-            for file in files:
+    for directory in dirs:
+        if (directory.isdigit and os.path.isdir(directory)):
+            # get .md files in this directory
+            for file in os.listdir(directory):
                 if file.endswith(".md"):
-                    with open(os.path.join(root, file), "r") as src:
+                    with open(os.path.join(directory, file), "r") as src:
                         src_md = src.read()
                         if not os.path.exists("content/posts"):
                             os.makedirs("content/posts")
                         with open(f"content/posts/{file}", "w") as post:
                             post.write("+++\n")
                             post.write(f"title = \"{file[:-3]}\"\n")
-                            post.write("description = \""+ read_first_line(os.path.join(root,file)) + "\"\n")
+                            post.write("description = \""+ read_first_line(os.path.join(directory,file)) + "\"\n")
                             post.write("+++\n")
                             post.write("\n")
                             post.write(src_md)
                             post.write("\n")
-
+                            
 if __name__ == "__main__":
     generate_posts()
